19a57e282ca18f608c1ec14a61b8360e5726929e
Magus <noah.m.jorgenson@gmail.com>
2011-11-15 16:57:26 -0600

    adding static files for skyrim subdomain hosted on heroku

---
+++ skyrim/static/js/script.js
@@ -0,0 +1,28 @@
+// Author: Noah M. Jorgenson
+
+$(function(){
+	$('.ingredient-box .display-all').click(function(){
+		$(this).toggleClass('selected');
+		var $checkbox = $(this).find('input:checkbox');
+       $checkbox.attr('checked', !$checkbox.attr('checked'));
+	});
+	
+	$('.ingredient-box .search-button').click(function(){
+		$('#ingredient-form').submit();
+	});
+	
+	$('.recipe-results-nav-button').click(function(){
+		var id = $(this).attr('target');
+		//hide all results
+		$('#recipe-results').children(':visible').fadeOut();
+		//display the targetted effect count recipe results
+		$("#"+id).fadeIn();
+	});
+	
+});
+
+
+
+
+
+
